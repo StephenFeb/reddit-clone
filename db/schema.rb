70171d1dd101aaaba3af0a0628fac824152058b9
StephenFeb <spinshon@hotmail.com>
2020-07-30 22:37:53 +0100

    5th commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_201558) do
+ActiveRecord::Schema.define(version: 2020_07_30_151415) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2020_07_22_201558) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
